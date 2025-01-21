200d486a88a11300521e104e688b9ed3aea4f92a
nur-ilayda-kaplan <nurilaydakargi@gmail.com>
2025-01-21 19:34:11 +0300

    S15D1 Completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import org.example.mobile.Contact;
+import org.example.mobile.MobilePhone;
+import org.example.models.Grocery;
+
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
+
+        //Grocery grocery= new Grocery();
+       //grocery.startGrocery();
+        MobilePhone mobilePhone=new MobilePhone("23232320",new ArrayList<>());
+        mobilePhone.addNewContact(new Contact("ilayda","232323232"));
+
+        mobilePhone.printContacts();
 
     }
 }
